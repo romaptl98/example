f178853735fa53158c5eb345087cfc8e276cb975
romaptl98 <romaptl98@gmail.com>
2021-03-14 01:01:49 +0530

    mybio

---
+++ MyBio.java
@@ -0,0 +1,21 @@
+/**
+*Name-roma patel
+*description-compilation and running test
+*date-13/3/21
+*/
+
+class MyBio{
+	public static void main(String [] args){
+		int personId=100;
+		String personName="Roma Patel";
+		String location="raigarh";
+		String contactNumber="7987815995";
+		String email="roma.ptl98@gmail.com";
+		
+		System.out.println("id : "+personId);
+		System.out.println("name : " +personName);
+		System.out.println("location : "+location);
+		System.out.println("Contact : "+contactNumber);
+		System.out.println("Email : "+email);
+	}
+}
